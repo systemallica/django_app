d198ca1db824e02b5be9a409dc4d9693adb29926
systemallica <andressser@gmail.com>
2018-02-02 17:22:09 +0100

    Improved admin page

--- djangoapp/urls.py
+++ djangoapp/urls.py
@@ -15,10 +15,8 @@ Including another URLconf
 """
 from django.urls import include, path
 from django.contrib import admin
-from .views import redirect_root
 
 urlpatterns = [
-	path('', redirect_root),
 	path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
 ]
